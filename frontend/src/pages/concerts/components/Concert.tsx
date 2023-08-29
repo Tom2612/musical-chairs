6b1603b26769acca603c408b51d32938e06f8cde
tom <tomcat-26@hotmail.co.uk>
2023-08-29 14:08:03 +0100

    Added navbar.tsx

--- frontend/src/pages/concerts/components/Concert.tsx
+++ frontend/src/pages/concerts/components/Concert.tsx
@@ -15,13 +15,13 @@ const Concert: React.FC<IProps> = ({ concerts }) => {
                     {/* <h4>{concert.group.name}</h4> */}
                     <h3>Date: {format(new Date(concert.date), 'PP')}</h3>
                     <h2>{concert.location}</h2>
-                    <h2>{concert.payStatus ? 'Paid' : 'Unpaid'}</h2>
+                    <p>{concert.payStatus ? 'Paid' : 'Unpaid'}</p>
                     <ul>
-                        <h3>Pieces:</h3>
+                        <h3>Programme:</h3>
                         {renderPieces(concert.pieces)}
                     </ul>
                     <ul>
-                        <h3>Instruments:</h3>                       
+                        <h3>Looking for:</h3>                       
                         {renderInstruments(concert.instruments)}
                     </ul>
                     <p>Posted: {formatDistanceToNow(new Date(concert.createdAt), {addSuffix: true})}</p>
