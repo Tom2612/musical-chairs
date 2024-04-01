bce8d2e554ccd5d7a7a7afb9d50421d5847532d6
tom <tomcat-26@hotmail.co.uk>
2024-04-01 15:15:02 +0100

    layout navbar

--- frontend/src/app/Layout.tsx
+++ frontend/src/app/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <div className='min-h-screen min-w-screen'>
+    <div className='min-h-screen min-w-screen text-gray-900'>
         <Header />
         <main className='p-10'>
             <Outlet></Outlet>
@@ -13,6 +13,13 @@ export default function Layout() {
 
 function Header() {
     return (
-        <div>Header</div>
+        <div className='p-5 bg-blue-300 flex items-center justify-center'>
+            <h1 className='text-3xl font-bold'>Musical Chairs</h1>
+            <div className='flex-1 flex justify-center space-x-5'>
+                <a href='/concerts'>Concerts</a>
+                <a href='/concerts/new'>New Conert</a>
+            </div>
+            <div>User</div>
+        </div>
     )
 }
