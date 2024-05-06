17b46d71f994927d61ab2f743beb3d05fabd04e5
tom <tomcat-26@hotmail.co.uk>
2024-05-06 19:50:08 +0100

    choice selection

--- frontend/src/forms/ChairForm.tsx
+++ frontend/src/forms/ChairForm.tsx
@@ -8,6 +8,7 @@ export default function ChairForm() {
         level: '',
         grade: undefined
     });
+    const [selectChoice, setSelectChoice] = useState(true);
     const params = useParams();
     const { id } = params;
 
@@ -37,20 +38,28 @@ export default function ChairForm() {
             <h2>{id === 'new' ? 'Add' : 'Edit'} Chair</h2>
             <label htmlFor='instrument'>Instrument</label>
             <input type='text' name='instrument' value={chair.instrument} onChange={handleChange} />
-            <label htmlFor='level'>Level</label>
-            <select name='level' value={chair.level} onChange={handleChange}>
-                <option value=''>-----</option>
-                {levels.map(level => (
-                    <option value={level}>{level}</option>
-                ))}
-            </select>
-            <label htmlFor='grade'>Grade</label>
-            <select name='grade' value={chair.grade} onChange={handleChange}>
-                <option value=''>-----</option>
-                {Array.from({ length: 8 }, (_, index) => (
-                    <option key={8 - index} value={8 - index}>{8 - index}+</option>
-                ))}
-            </select>
+            {selectChoice && 
+                <>
+                    <label htmlFor='level'>Level</label>
+                    <select name='level' value={chair.level} onChange={handleChange}>
+                        <option value=''>-----</option>
+                        {levels.map(level => (
+                            <option value={level}>{level}</option>
+                        ))}
+                    </select>
+                </>
+            }
+            {!selectChoice && 
+                <>
+                    <label htmlFor='grade'>Grade</label>
+                    <select name='grade' value={chair.grade} onChange={handleChange}>
+                        <option value=''>-----</option>
+                        {Array.from({ length: 8 }, (_, index) => (
+                            <option key={8 - index} value={8 - index}>{8 - index}+</option>
+                        ))}
+                    </select>
+                </>
+            }
         </form>
     )
 }
